85e1a5445a02819126b42aa49ceb0a040a66c8b8
Ilya Dymshits <ilya@devlion.co>
2018-11-19 14:10:59 +0200

    (update) pinned section

--- amd/src/pinned.js
+++ amd/src/pinned.js
@@ -20,11 +20,15 @@ const refreshPinnedSection = () => {
   });
 }
 
-const addPinnedSection = (target) => {
+const getSectionId = (target) => {
   while(!target.classList.contains(`section`)) {
     target = target.parentNode;
   }
-  let sectionnumber = target.id.replace(/\D+/,'');
+  return sectionId = target.id.replace(/\D+/,'');
+}
+
+const addPinnedSection = (target) => {
+  let sectionnumber = getSectionId(target);
   let targetSection = document.querySelector(`a[id=gridsection-${sectionnumber}]`);
   targetSection.parentNode.classList.add('pinned');
 
@@ -32,10 +36,7 @@ const addPinnedSection = (target) => {
 }
 
 const removePinnedSection = (target) => {
-  while(!target.classList.contains(`section`)) {
-    target = target.parentNode;
-  }
-  let sectionnumber = target.id.replace(/\D+/,'');
+  let sectionnumber = getSectionId(target);
   let targetSection = document.querySelector(`a[id=gridsection-${sectionnumber}]`);
   targetSection.parentNode.classList.remove('pinned');
 
@@ -50,8 +51,29 @@ const removePinnedSection = (target) => {
  * @returns {Integer}
  */
 const togglePinnedSectionIndicator = (target) => {
-   let buttonIndicator = target.firstChild;
-   buttonIndicator.title;
+  let pinbefore;
+  let pinafter;
+  if(target.dataset.action === `topinsection`){
+    pinbefore = 1;
+    pinafter = 0;
+  } else {
+    pinbefore = 0;
+    pinafter = 1;
+  }
+    // item.href = item.href.replace('pinned=1', 'pinned=0');
+    // item.title = "This section is pinned";
+    // item.dataset.action = "tounpinsection";
+    // item.childNodes[0].classList = "icon fa fa-unlock fa-fw ";
+    // item.childNodes[0].title = "This section is pinned";
+    // item.childNodes[1].innerText = "Unpin section";
+    // var section = getClosest(item, 'li.section');
+    // var sectionIndex = section.id.replace( /\D+/g, '');
+    // document.querySelector('[aria-labelledby="gridsectionname-'+ sectionIndex +'"]').classList = 'pinned';
+    // pinned = document.querySelectorAll(`.gridicons .pinned`);
+    // pinnedGridLayout(wrapper, pinned, pinnedBlock);
+    target.href = "atik";
+    // document.querySelector(`a[id=section-${sectionnumber}] `).href = target.href.replace(`pinned=${pinbefore}`, `pinned=${pinafter}`);
+
 }
 
 /**
@@ -79,11 +101,13 @@ const checkMaxCountOfPinnedSection = () => {
                   // modal.setModal();
                   return;
                 }
+                togglePinnedSectionIndicator(target);
                 addPinnedSection(target);
                 return
               }
               // unset pinned section
               if (target.dataset.action === `tounpinsection`) {
+                togglePinnedSectionIndicator(target);
                 removePinnedSection(target);
                 return
               }
